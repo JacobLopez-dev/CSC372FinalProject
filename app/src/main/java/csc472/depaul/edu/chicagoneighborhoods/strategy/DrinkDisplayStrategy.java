d77bdd37695d1776574bd5aad4c00bfb9fe0b5fd
LAPTOP-J2PV8AJL\Jim Brehm <jim.brehm.55@gmail.com>
2018-11-18 19:16:34 -0600

    Food layer added

--- app/src/main/java/csc472/depaul/edu/chicagoneighborhoods/strategy/DrinkDisplayStrategy.java
+++ app/src/main/java/csc472/depaul/edu/chicagoneighborhoods/strategy/DrinkDisplayStrategy.java
@@ -3,7 +3,6 @@ package csc472.depaul.edu.chicagoneighborhoods.strategy;
 import android.content.Context;
 
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
 
 public class DrinkDisplayStrategy implements MapDisplayStrategy {
     @Override
